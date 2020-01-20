21b78997b1765641f5a8bfc036a1fafbe456572e
ricky wahba <rjwahba@gmail.com>
2020-01-20 02:37:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,11 @@ def get_japanese_emoticon(file, emoticon)
   end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file, emoticon)
+  emoticons = load_library(file)
+  if emoticons[:get_emoticon].keys.include?(emoticon)
+    return emoticons[:get_emoticon][emoticon]
+  else
+    return "Sorry, that emoticon was not found"
+  end
 end
