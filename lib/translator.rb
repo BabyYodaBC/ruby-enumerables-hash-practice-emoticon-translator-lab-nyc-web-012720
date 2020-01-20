54ed4241b464a21227fc2332ecda995453b0b606
ricky wahba <rjwahba@gmail.com>
2020-01-20 02:26:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,10 +5,9 @@ def load_library(file)
   hash = {"get_emoticon": {}, "get_meaning": {}}
   emoticons.each do |meaning, moti|
     hash[get_meaning][moti[1]] = meaning
-    hash[get_meaning][moti[0]] = meaning
-    hash[get_emoticon][moti[0]] = 
+    hash[get_emoticon][moti[0]] = meaning
   end
-  # print hash
+  print hash
   #lib = {get_meaning: emoticons.keys, get_emoticon: emoticons.values}
   return hash
 end
