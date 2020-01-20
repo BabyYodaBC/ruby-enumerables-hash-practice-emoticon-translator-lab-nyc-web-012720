0b6f9d0e183378e68d60e013de68f0b7b7803964
ricky wahba <rjwahba@gmail.com>
2020-01-20 02:11:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,6 +3,7 @@ require "yaml"
 def load_library(file)
   emoticons = YAML.load_file(file)
   lib = {get_meaning: emoticons.keys, get_emoticon: emoticons.values}
+  lib
 end
 
 def get_japanese_emoticon
